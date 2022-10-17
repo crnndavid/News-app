41c636723433e016b3c65110324735bad9898ac0
crnndavid <95691870+crnndavid@users.noreply.github.com>
2022-10-17 14:11:32 +0200

    Global styling

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>News App</title>
-        <meta name="description" content="News APp" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </div>
-  );
+  return <div className="">Hello World</div>;
 }
